e01d0fc8a77cdc0995d9562f8d7d810494fdb604
Sudeep Hosur <159408755+Gawdiee@users.noreply.github.com>
2025-09-08 00:43:58 +0530

    ippclient.py

--- client1.py
+++ client1.py
@@ -1,23 +1,27 @@
 import socket
 import select
 import sys
+import ssl
+
+
+context = ssl.create_default_context()
+
+context.load_verify_locations(cafile='ca-cert.pem')
+
+server = context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname='192.168.233.114')
 
 MSG_LEN = 5
 
-if len(sys.argv) != 3:
-    print("Correct method: script, host IP address, port number")
-    exit()
-
-Host = str(sys.argv[1])
-Port = int(sys.argv[2])
+Host = '192.168.233.114' #paste you ip address here
+Port = 12345
 
 name = input("Enter Name: ")
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     server.connect((Host, Port))
-except Exception as e:
-    print("CAN'T CONNECT TO SERVER:", e)
+except:
+    print("CAN'T CONNECT TO SERVER!")
     sys.exit()
 
 server.setblocking(False)
@@ -30,27 +34,47 @@ def send_name_to_server(socket, message):
     else:
         try:
             socket.send(bytes(message, 'utf-8'))
-        except Exception as e:
-            print("Error sending name to server:", e)
+        except:
             socket.close()
-            sys.exit()
+            clients_list.remove(receiver)
 
-# Remaining functions (send_to_one, receive_message) are unchanged
+def send_to_one(socket, message):
+    try:
+        socket.send(bytes(message, 'utf-8'))
+    except:
+        socket.close()
+        clients_list.remove(receiver)
+
+def receive_message(client_socket):
+    try:
+        msg_len = client_socket.recv(MSG_LEN)
+
+        if not len(msg_len):
+            return False
+
+        message_length = int(msg_len.decode('utf-8').strip())
+        return {'Length': msg_len, 'data': client_socket.recv(message_length)}
+
+    except:
+        return False
 
 send_name_to_server(server, name)
 
 while True:
     sockets_list = [sys.stdin, server]
-    read_sockets, _, _ = select.select(sockets_list, [], [])
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
     for socket in read_sockets:
         if socket == server:
             encoded_message = receive_message(socket)
-            if not encoded_message:
+            if not encoded_message :
                 print("DISCONNECTED")
                 sys.exit()
             else:
                 message = encoded_message['data'].decode('utf-8')
                 print(message)
+
         else:
             message = sys.stdin.readline()
             send_to_one(server, message)
+server.close()
+sys.exit()
